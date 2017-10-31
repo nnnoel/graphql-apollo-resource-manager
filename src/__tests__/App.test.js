c3de00b4fb628da87e1f2c9230011dd14b17e689
Noel <ncolon20@gmail.com>
2017-10-30 23:18:29 -0400

    fix tests for App comp

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,39 +1,35 @@
 import React from 'react';
-import { render as renderToDOM } from 'react-dom';
+import { createStore } from 'redux';
+import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-client';
-import { mockNetworkInterface } from 'react-apollo/test-utils';
-import { setupStore } from '../store';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import App from '../App';
 
 describe('Resource Manager app', () => {
   it('should initially render the programs list directory', () => {
-    const node = document.createElement('div');
     const client = new ApolloClient({
-      networkInterface: mockNetworkInterface(
-        ...[
-          {
-            request: jest.fn(),
-            result: jest.fn()
-          }
-        ]
-      )
+      link: createHttpLink({ uri: 'test' }),
+      cache: new InMemoryCache()
     });
-    const store = setupStore(jest.fn());
-
-    renderToDOM(
-      <ApolloProvider client={client} store={store}>
-        <MemoryRouter initialEnties={['/']}>
-          <App />
-        </MemoryRouter>
-      </ApolloProvider>,
-      node
+    const store = createStore(() => ({}));
+    const wrapper = shallow(
+      <Provider store={store}>
+        <ApolloProvider client={client}>
+          <MemoryRouter initialEntries={['/']}>
+            <App />
+          </MemoryRouter>
+        </ApolloProvider>
+      </Provider>
     );
 
-    expect(node.innerHTML).toContain('Search for Programs');
-    expect(node.innerHTML).toContain('<span>Filter By:</span>');
-    expect(node.innerHTML).toContain('footer');
-    expect(node.innerHTML).toContain('© 2017 Noel Colon');
+    expect(wrapper.html()).toContain('Search for Programs');
+    expect(wrapper.html()).toContain('<span>Filter By:</span>');
+    expect(wrapper.html()).toContain('id="program-list-section"');
+    expect(wrapper.html()).toContain('footer');
+    expect(wrapper.html()).toContain('© 2017 Noel Colon');
   });
 });
